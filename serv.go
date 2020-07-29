597372b8eb56e30182d7f0f7710e5e9e6cc9ceaf
Viatcheslav Bugaev <v..b..@gmail>
2020-07-29 17:39:17 -0500

    First commit.

---
+++ serv.go
@@ -0,0 +1,238 @@
+package main
+
+// Helpfull snippets:
+// /data/bugaev/PROG/GO/POST/second.html
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "strings"
+    "strconv"
+    "os"
+    "net"
+    "text/template"
+    // "bytes"
+    // "os"
+    // "os/exec"
+)
+
+var SelectUploadFileHtmlTmpl *template.Template
+
+func LimitUpload40MB(r *http.Request) {
+    // Parse our multipart form, 10 << 22 specifies a maximum
+    // upload of 40 MB files.
+    r.ParseMultipartForm(10 << 22)
+}
+
+func ShakyVidBytes(r *http.Request) ([]byte, bool) {
+    // FormFile returns the first file for the given key `myFile`
+    // it also returns the FileHeader so we can get the Filename,
+    // the Header and the size of the file
+    file, header, err := r.FormFile("myFile")
+    if err != nil {
+        fmt.Println("Error Retrieving the File")
+        fmt.Println(err)
+        return nil, false
+    }
+    defer file.Close() // In case reading to memory fails.
+    fmt.Printf("Uploaded File: %+v\n", header.Filename)
+    fmt.Printf("File Size: %+v\n", header.Size)
+    fmt.Printf("MIME Header: %+v\n", header.Header)
+
+
+    // read all of the contents of our uploaded file into a
+    // byte array
+    fileBytes, err := ioutil.ReadAll(file)
+    if err != nil {
+        fmt.Println(err)
+	return nil, false
+    }
+    return fileBytes, true
+}
+
+type MySess struct {
+  w http.ResponseWriter
+  r *http.Request
+  TmpDir string
+  shaky_vid string
+  stabi_vid string
+  stabi_vid_full_path string
+  ID string
+}
+
+
+func ArrLastStr(str []string) string {
+  return str[len(str) - 1]
+}
+
+func (s *MySess) WorkDir2ID() *MySess {
+  DirParts := strings.Split(s.TmpDir, "/")
+  s.ID = ArrLastStr(DirParts)
+  fmt.Println("Session ID: ", s.ID)
+  return s
+}
+
+
+func (s *MySess) MkTmpDir () (*MySess, error) {
+    var err error
+    s.TmpDir, err = ioutil.TempDir("WORKDIR", "")
+    fmt.Println("Temporary directory is:", s.TmpDir, "<--")
+    if err != nil {
+        fmt.Println(err)
+        return s, err
+    }
+    return s, nil
+}
+
+func (s *MySess) ShakyVidHdd (fileBytes []byte) (*MySess, error) {
+    // Create input file "shaky.mp4" within WORKDIR:
+    s.shaky_vid = s.TmpDir + "/shaky.mp4"
+    err := ioutil.WriteFile(s.shaky_vid, fileBytes, 0644)
+    if err != nil {
+      fmt.Println(err)
+    }
+    return s, err
+}
+
+func (s *MySess)  TmpDir2stabi_vid_full_path() (*MySess, error) {
+    // s.stabi_vid_full_path = s.TmpDir + "/" + stabi_vid
+    s.stabi_vid_full_path = "WORKDIR" + "/" + s.stabi_vid
+    return s, nil
+}
+
+// Based on: https://tutorialedge.net/golang/go-file-upload-tutorial/
+// TODO: Why is w not a pointer?
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("File Upload Endpoint Hit")
+
+    var err error
+
+    Sess := &MySess{
+      w: w,
+      r: r,
+      stabi_vid: "stabi_dummy.mp4",
+      // Plus uninitialized fields.
+    }
+
+
+    _, err = Sess.MkTmpDir(); if err != nil { fmt.Printf("Error returned by MkTmpDir: %s\n", err); return }
+    Sess.TmpDir2stabi_vid_full_path()
+    Sess.WorkDir2ID()
+
+//    return
+
+    LimitUpload40MB(r)
+    fileBytes, ok := ShakyVidBytes(r); if !ok { return }
+    _, err = Sess.ShakyVidHdd(fileBytes); if err != nil { return }
+
+    // fmt.Fprintf(w, "Starting analysis\n")
+//    ffmpeg_exec, _ := exec.LookPath( "ffmpeg" )
+//    cmd := &exec.Cmd {
+//      Path:  ffmpeg_exec,
+//      Args: []string{ ffmpeg_exec, "-i", Sess.TmpDir + "/shaky.mp4", "-vf", "vidstabdetect=shakiness=10:accuracy=15:result=mytransforms.trf", "-f", "null", "-" },
+//      Stdout: os.Stdout,
+//      Stderr: os.Stdout,
+//    }
+
+    // if err := cmd.Run(); err != nil {
+    //   fmt.Println( "Error: ", err );
+    // }
+// https://stackoverflow.com/questions/24116147/how-to-download-file-in-browser-from-go-server
+   w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote(Sess.stabi_vid))
+   w.Header().Set("Content-Type", "application/octet-stream")
+   http.ServeFile(w, r, Sess.stabi_vid_full_path)
+}
+
+func RequestedHostByClient(r *http.Request) string {
+    return r.Host
+}
+
+func SelectUploadFile(w http.ResponseWriter, r *http.Request) {
+    // var buf bytes.Buffer
+    // err := SelectUploadFileHtmlTmpl.Execute(&buf, MyURL{AddrWithPortSeenByClient: RequestedHostByClient(r)}); if err != nil { panic(err) }
+    // fmt.Fprintf(w, buf.String())
+    err := SelectUploadFileHtmlTmpl.Execute(w, MyURL{AddrWithPortSeenByClient: RequestedHostByClient(r)}); if err != nil { panic(err) }
+}
+
+func setupRoutes() {
+    http.HandleFunc("/upload", uploadFile)
+    http.HandleFunc("/", SelectUploadFile)
+    http.ListenAndServe(":8080", nil)
+}
+
+func ListAllHostIP() {
+    ifaces, err := net.Interfaces()
+    if err != nil { return }
+    // handle err
+    for _, i := range ifaces {
+        addrs, err := i.Addrs()
+        if err != nil { return }
+        for _, addr := range addrs {
+            var ip net.IP
+            switch v := addr.(type) {
+            case *net.IPNet:
+                    ip = v.IP
+            case *net.IPAddr:
+                    ip = v.IP
+            }
+            fmt.Println(ip.String())
+        }
+    }
+}
+
+
+func FirstHostIP4() (string, bool) {
+// https://gist.github.com/jniltinho/9787946
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+       os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+       os.Exit(1)
+    }
+
+    for _, a := range addrs {
+        if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                // os.Stdout.WriteString(ipnet.IP.String() + "\n")
+                return ipnet.IP.String(), true
+             }
+        }
+    }
+    return "none", false
+}
+
+type MyURL struct {
+    AddrWithPortSeenByClient string
+}
+
+func NewSelectUploadFileHtmlTmpl() {
+    var err error
+    SelectUploadFileHtmlTmpl, err = template.New("SelectUploadFileHtml").Parse(
+`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+  </head>
+  <body>
+    <form
+      enctype="multipart/form-data"
+      action="http://{{.AddrWithPortSeenByClient}}/upload"
+      method="post"
+    >
+      <input type="file" name="myFile" />
+      <input type="submit" value="upload" />
+    </form>
+  </body>
+</html>`)
+    if err != nil { panic(err) }
+}
+
+func main() {
+    fmt.Println("Hello World")
+    NewSelectUploadFileHtmlTmpl()
+    setupRoutes()
+}
+
